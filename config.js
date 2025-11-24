10319875f50202253836b7583465d5539860bfd4
Muhammad Rizky <aksobonde3@gmail.com>
2025-11-24 09:58:15 +0800

    Change default language from Spanish to English

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.authFile = `MysticSession`;
 // Error de m.isBaileys marcado como false fix temporal
 global.isBaileysFail = false;
 
-global.defaultLenguaje = 'es';
+global.defaultLenguaje = 'en';
 
 global.owner = [
   ['6285656715298', '👑 33333 👑', true],
